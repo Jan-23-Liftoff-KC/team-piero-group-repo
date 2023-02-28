8feb99d96ca14a08bdb4bb88a091a411b9e73666
fraagu10 <aguilarfranco1092@protonmail.com>
2023-02-28 15:13:44 -0600

    updated code to redirect user to profile page after login

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { auth } from 'src/firebase/firebase.init';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  constructor() { }
+
+  user = auth.currentUser;
+
+  constructor(private matSnackBar: MatSnackBar, private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  signOut() {
+    auth.signOut().catch((err) => console.log(err));
+    this.matSnackBar.open("signed out successfully", "CLOSE");
+    this.router.navigate(["landing"]);
+  }
+
 }
